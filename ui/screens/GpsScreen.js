f533004caf8e08886e539f65843a7499e1902290
Quinn <qcasey1@gmail.com>
2019-10-15 14:57:56 -0700

    normal text for gps

--- ui/screens/GpsScreen.js
+++ ui/screens/GpsScreen.js
@@ -114,11 +114,11 @@ export default class GpsScreen extends React.Component {
 						</View>
 
 						<View style={[styles.container, styles.containerPaddingLeft, styles.containerPaddingRight, styles.containerPaddingBottom, styles.colContainer]}>
-							<Text style={styles.auxText}>Latitude: {this.state.region.latitude}</Text>
-							<Text style={styles.auxText}>Longitude: {this.state.region.longitude}</Text>
-							<Text style={styles.auxText}>Time: {this.state.gps.time}</Text>
-							<Text style={styles.auxText}>Speed: {this.state.gps.speed}</Text>
-							<Text style={styles.auxText}>Course: {this.state.gps.course}</Text>
+							<Text style={styles.normalText}>Latitude: {this.state.region.latitude}</Text>
+							<Text style={styles.normalText}>Longitude: {this.state.region.longitude}</Text>
+							<Text style={styles.normalText}>Time: {this.state.gps.time}</Text>
+							<Text style={styles.normalText}>Speed: {this.state.gps.speed}</Text>
+							<Text style={styles.normalText}>Course: {this.state.gps.course}</Text>
 						</View>
 					</View>
 				</View>
